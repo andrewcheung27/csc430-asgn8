adbd90547f57c63b18458bfd632f999a66892a9e
Saanvi Dua <76962646+saanvidua@users.noreply.github.com>
2023-06-07 20:23:50 -0700

    unit test for numc interp

--- spec/vvqs_spec.cr
+++ spec/vvqs_spec.cr
@@ -1,2 +1,12 @@
 require "spec"
+require "./vvqs"
 
+
+describe "interp" do
+  it "interprets a NumC expression" do
+    env = Environment.new([] of Binding)
+    exp = NumC.new(42)
+    result = interp(exp, env)
+    result.should eq(NumV.new(42))
+  end
+end
